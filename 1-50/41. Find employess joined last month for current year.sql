85299bdf01daad0f835bd0862b26a26ed852fca6
Sourabh Sao <133841512+Sourabh-Sao@users.noreply.github.com>
2026-01-23 21:19:39 +0530

    Enhance SQL query to find employees joined last month
    Added an alternative query to find employees hired last month using a different date range method.

--- 1-50/41. Find employess joined last month for current year.sql
+++ 1-50/41. Find employess joined last month for current year.sql
@@ -3,3 +3,10 @@ select *
 from employees
 where month(hire_date)=month(DATE_SUB((CURRENT_DATE),INTERVAL 1 MONTH))
 and YEAR(hire_date)=YEAR(CURRENT_DATE)
+
+--or
+
+SELECT *
+FROM employees
+WHERE hire_date >= LAST_DAY(CURRENT_DATE() - INTERVAL 2 MONTH) + INTERVAL 1 DAY
+  AND hire_date <= LAST_DAY(CURRENT_DATE() - INTERVAL 1 MONTH);
